989b534752498a8732abc979f16e71b6099974c1
davidcrc <david_drcr@hotmail.com>
2024-09-05 16:30:42 -0500

    feat: add menus and container from spatial-navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,105 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled, { createGlobalStyle } from "styled-components";
+import { Menu } from "./components/menu/menu";
+import { Content } from "./components/content/content";
+import { shuffle } from "lodash";
+import { IAsset } from "./components/content/asset";
+
+const menuItems = [
+  "Home",
+  "Movies",
+  "TV Shows",
+  "Sports",
+  "Music",
+  "News",
+  "Kids",
+  "Lifestyle",
+  "Entertainment",
+  "More",
+];
+
+export const assets: IAsset[] = [
+  {
+    title: "Asset 1",
+    color: "#714ADD",
+  },
+  {
+    title: "Asset 2",
+    color: "#AB8DFF",
+  },
+  {
+    title: "Asset 3",
+    color: "#512EB0",
+  },
+  {
+    title: "Asset 4",
+    color: "#714ADD",
+  },
+  {
+    title: "Asset 5",
+    color: "#AB8DFF",
+  },
+  {
+    title: "Asset 6",
+    color: "#512EB0",
+  },
+  {
+    title: "Asset 7",
+    color: "#714ADD",
+  },
+  {
+    title: "Asset 8",
+    color: "#AB8DFF",
+  },
+  {
+    title: "Asset 9",
+    color: "#512EB0",
+  },
+];
+
+const rows = shuffle([
+  {
+    title: "Recommended",
+    assets: assets,
+  },
+  {
+    title: "Movies",
+    assets: assets,
+  },
+  {
+    title: "Series",
+    assets: assets,
+  },
+  {
+    title: "TV Channels",
+    assets: assets,
+  },
+  {
+    title: "Sport",
+    assets: assets,
+  },
+]);
+
+const AppContainer = styled.div`
+  background-color: #221c35;
+  width: 100%;
+  height: 810px;
+  display: flex;
+  flex-direction: row;
+`;
+
+const GlobalStyle = createGlobalStyle`
+  ::-webkit-scrollbar {
+    display: none;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <GlobalStyle />
+      <Menu focusKey="MENU" items={menuItems} />
+      <Content rows={rows} focusKey="CONTENT" />
+    </AppContainer>
   );
 }
 
